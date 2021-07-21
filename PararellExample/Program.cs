30b16740e016b905a586a153c8e2bbaae1ed601b
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-21 14:04:27 +0200

    update

---
+++ PararellExample/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PararellExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LoopNoPararell();
+            LoopWithPararell();
+        }
+
+        public static void LoopNoPararell()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                long total = LongOperation();
+                Console.WriteLine("{0} - {1}", i, total);
+            }
+            sw.Stop();
+            Console.WriteLine("LoopNoPararell - {0}", sw.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+
+        public static void LoopWithPararell()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 8
+            };
+            //Console.WriteLine($"{options.MaxDegreeOfParallelism}");
+            //options.MaxDegreeOfParallelism = 2;
+
+            Parallel.For(0, 10, options, i => {
+                long total = LongOperation();
+                Console.WriteLine("{0} - {1}", i, total);
+            });
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    long total = LongOperation();
+            //    Console.WriteLine("{0} - {1}", i, total);
+            //}
+            sw.Stop();
+            Console.WriteLine("LoopWithPararell - {0}", sw.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+
+
+        public static long LongOperation()
+        {
+            long total = 0;
+            for (int i = 0; i < 100_000_000; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+    }
+}
---
+++ ThreadPoolExample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ThreadPool.SetMaxThreads(1, 1);
+            //ThreadPool.SetMinThreads(1, 1);
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(new WaitCallback(MyCustomMethod));
+            //}
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            MethodWithThread();
+            sw.Stop();
+            Console.WriteLine($"MethodWithThread - {sw.ElapsedMilliseconds}");
+
+            sw.Restart();
+            MethodWithThreadPool();
+            sw.Stop();
+            Console.WriteLine($"MethodWithThreadPool - {sw.ElapsedMilliseconds}");
+
+            Console.ReadKey();
+        }
+
+
+        public static void MethodWithThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(Test);
+                thread.Start();
+            }
+        }
+
+        public static void MethodWithThreadPool()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Test));
+            }
+        }
+
+        public static void Test(object obj)
+        {
+
+        }
+
+        public static void MyCustomMethod(object obj)
+        {
+            Thread thread = Thread.CurrentThread;
+            string msg = $"Thread Pool: {thread.IsThreadPoolThread}, ThreadID: {thread.ManagedThreadId}";
+            Console.WriteLine(msg);
+            Thread.Sleep(1000);
+        }
+    }
+}
